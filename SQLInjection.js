14bd643850c48dcd12c1341d58a7ae1313a76104
Laurent Huang <laurenthuang@hotmail.fr>
2019-11-07 22:55:59 +0800

    redirection in case of false password

--- SQLInjection.js
+++ SQLInjection.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var db = require('./db/login.js');
-var app = express(); // creation du serveur
+var app = express(); 
 var session = require('express-session');
 
 var app = express();
@@ -11,20 +11,20 @@ app.get ( '/' , function ( req , res ) {
 });
 
 app.post('/log',function(req,res){
-    var username = req.body.username; 
-    var password = req.body.password;
-    var query = "SELECT secret FROM user where username = '" + username + "' and password = '" + password + "'";
-
+    var username = req.username; 
+    var password = req.password;
     console.log("username: " + username);
     console.log("password: " + password);
+    var query = "SELECT secret FROM user where username = '" + username + "' and password = '" + password + "'";
+
     console.log('query: ' + query);
 
     getLogin ( query, 
     function ( error , data ){
       if ( error == null )
-        res.send ('Your secret: data.secret');
+        res.send ('Your secret:'+  data.secret+ '<p><a href="path.join(__dirname+\'index.html\')">Go back to login</a></p>)');
 	  else if(!data)
-		res.redirect("/index.html#false");
+	    res.sendFile(path.join(__dirname + '/indexFalse.html'));
       else
         res.render ( 'error' , { message : error }) ;
     })
