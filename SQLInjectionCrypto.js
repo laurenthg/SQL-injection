60ad41b008c9778d30d8303bdcb367ebf56ea6ba
Laurent Huang <laurenthuang@hotmail.fr>
2019-11-08 21:17:00 +0800

    crypted username and password in logincrypto table

---
+++ SQLInjectionCrypto.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var db = require('./db/login.js');
+var app = express(); 
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var SimpleCrypto = require("simple-crypto-js").default;
+var _secretKey = 'secretKey'; 
+var simpleCrypto = new SimpleCrypto(_secretKey);
+var sha256 = require('js-sha256');
+var app = express();
+app.use(bodyParser.urlencoded({ extended : true }));// use for get 'name' from HTML to app.post
+
+app.get ( '/' , function ( req , res ) {
+	    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/log',function(req,res){
+    var username = req.body.username; 
+    console.log("username: " + username);
+    var password = req.body.password;
+    console.log("password: " + password);
+	var hashUsername = sha256.create();
+	hashUsername.update(username);
+    console.log("username hash: " + hashUsername);
+	var hashPassword = sha256.update(password);
+    console.log("password hash: " + hashPassword);
+
+    var query = "SELECT secret FROM LOGINCRYPTO where username ='" + hashUsername + "' and password ='" + hashPassword+"'";
+
+    console.log('query: ' + query);
+
+    getLogin ( query, 
+    function ( error , data ){
+      if ( error == null ){
+		console.log(data[0]);
+		if (data[0]==undefined)  
+			res.sendFile(path.join(__dirname + '/indexFalse.html'));
+		else 
+			res.send ('Your secret:'+  data[0].secret+ '<p><a href=http://localhost:8080>Go back to login</a></p>)');
+	  }	  
+      else
+		res.json({error:error});
+    })
+
+  });
+
+console.log("start 8080");
+app.listen(8080);
+
--- init.sql
+++ init.sql
@@ -3,11 +3,19 @@
 --
 
 DROP TABLE LOGIN;
+DROP TABLE LOGINCRYPTO;
 CREATE TABLE LOGIN (
-    username character varying(200),
-    password character varying(200),
-	secret character varying(200),
+    username character varying(2000),
+    password character varying(2000),
+	secret character varying(2000),
+	PRIMARY KEY (username)
+);
+CREATE TABLE LOGINCRYPTO (
+    username character varying(2000),
+    password character varying(2000),
+	secret character varying(2000),
 	PRIMARY KEY (username)
 );
 
 INSERT INTO LOGIN(username,password,secret) VALUES('admin','adminPassword','0123456789');
+INSERT INTO LOGINCRYPTO(username,password,secret) VALUES('8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','0738a3e2111e114a665d35b24be5f42d1f98700f161633f63fbaa15b4b3103ab','0123456789');
