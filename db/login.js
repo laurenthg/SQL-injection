e499210a71bd74814ac061bfdde78d8ee19e6c6b
huangje <jerome.huang@hotmail.fr>
2019-11-09 18:29:04 +0800

    push login

--- db/login.js
+++ db/login.js
@@ -4,8 +4,30 @@ var dbconfig = require('../config/settings.js').settings
 
 var db = pgp(dbconfig)
 
-getLogin = function getLogin(SQLQuery,callback ) {
-    db.any ( SQLQuery, null )
+// getLogin = function getLogin(SQLQuery,callback ) {
+//     db.any ( SQLQuery, null )
+//       .then ( function( data ) {
+//           callback (null,data ) ;
+//       })
+//     .catch ( function(error ) {
+//       callback (error,null ) ;
+//     })
+// };
+//Injection
+// getLogin = function getLogin(username, password,callback ) {
+//     var query = "SELECT secret FROM LOGIN where username ='" + username + "' and password ='" + password+"'";
+//     db.any ( query, null )
+//       .then ( function( data ) {
+//           callback (null,data ) ;
+//       })
+//     .catch ( function(error ) {
+//       callback (error,null ) ;
+//     })
+// };
+//param query
+getLogin = function getLogin(username, password,callback ) {
+    var query = "SELECT secret FROM LOGIN where username = $1 and password = $2";
+    db.any ( query, [username, password] )
       .then ( function( data ) {
           callback (null,data ) ;
       })
@@ -13,6 +35,7 @@ getLogin = function getLogin(SQLQuery,callback ) {
       callback (error,null ) ;
     })
 };
+
 
 insertLogin = function insertLogin(SQLQuery,callback){
 	db.any(SQLQuery,null)
@@ -26,6 +49,5 @@ insertLogin = function insertLogin(SQLQuery,callback){
 
 module.exports = {
   getLogin : getLogin,
-  insertLogin : insertLogin	
+  insertLogin : insertLogin
 };
-
